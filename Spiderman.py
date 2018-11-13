54d9c0779ae1228c966e35a6cd144110c3e98927
Kenneth Maguire <kenneth.maguire@ucdenver.edu>
2018-11-12 20:54:58 -0700

    "just need to fix matmul for spidey # of 4"

--- Spiderman.py
+++ Spiderman.py
@@ -1,14 +1,18 @@
 
 import numpy as np
 import time as ti
-"""
-a = [[0,5], [10, 15], [11,100]]
+np.set_printoptions(threshold=8000)
+a = [[0,5,3], [10, 15,1], [11,100,7]]
 b = [[2,3], [5,4]]
-matmu = np.matmul(a,b)
+a = np.matrix(a)
+b = np.matrix(b)
 
-print(matmu)
 
-"""
+matmu1 = np.matmul(a,a)
+print(matmu1)
+
+matmu2 = a * a
+print(matmu2)
 #6486 charaters
 #spiderman 5306
 
@@ -33,7 +37,7 @@ with open("porgat.txt", "r", encoding="cp1252") as f:       #
 
 f.close()
 edges.remove(('*Edgeslist',))
-hcMatrix = np.zeros([len(heroes), len(comics)], dtype=np.int8)
+hcMatrix = np.zeros([len(heroes), len(comics)], dtype=np.int32)
 
 
 print("The length of the heroes list is: " + str(len(heroes)))
@@ -56,7 +60,8 @@ print("\n")
  #end of association matrix
 
  #start collab matrix
-clbMat = np.zeros([len(heroes),len(heroes)], dtype=np.int8)
+#clbMat = np.zeros([len(heroes),len(heroes)], dtype=np.int8)
+""" Collab matrix built, stored in matfile.txt. Building takes ~150s, loading from file takes 20s
 t0 = ti.time()
 for col in range(0,len(comics)):
     clbHero = []
@@ -73,11 +78,84 @@ print("Total time to build collab matrix: " + str(totTime))
 savClbMat = np.matrix(clbMat)
 with open('matfile.txt', 'w') as f:
     for line in savClbMat:
-        np.savetxt(f, line, fmt='i')
-
+        np.savetxt(f, line, fmt='%i')
+"""
 t0 = ti.time()
-spidey2 = np.matmul(clbMat, clbMat)
+with open('matfile.txt', 'r') as f:
+    clbMat = np.loadtxt(f, dtype=np.int32)
 t1 = ti.time()
+print(clbMat)
+totTime = t1 - t0
+print("Total time to build collab matrix: " + str(totTime))
+print(clbMat[5305])
+spidey2 = np.zeros([len(heroes), len(heroes)], dtype=np.int32)
+t0 = ti.time()
+print("getting spiderman of 2")
+spidey1 = clbMat[5305]
+spidey2 = np.matmul(clbMat,spidey1)
+print("getting spiderman of 3")
+spidey3 = np.matmul(clbMat,spidey2)
+print("getting spiderman of 4")
+spidey4 = np.matmul(clbMat,spidey3)
+
+t1 = ti.time()
+"""
+print("spidey 2")
+print(spidey2[5305])
+print("spidey 3")
+print(spidey3[5305])
+print("spidey 4")
+print(spidey4[5305])
+"""
+print(spidey1)
 print(spidey2)
+print(spidey3)
+print(spidey4)
+
 totTime = t1-t0
 print("Total time for matmul: " + str(totTime))
+
+print("spidey1")
+zeroTot = 0
+for i in range(0, len(heroes)):
+    if spidey1[i] == 0:
+        zeroTot += 1
+print(zeroTot)
+zeroTot = 0
+print("spidey2")
+for i in range(0, len(heroes)):
+    if spidey2[i] == 0:
+        zeroTot += 1
+print(zeroTot)
+zeroTot = 0
+print("spidey3")
+for i in range(0, len(heroes)):
+    if spidey3[i] == 0:
+        zeroTot += 1
+
+print(zeroTot)
+zeroTot = 0
+print("spidey4")
+for i in range(0, len(heroes)):
+    if spidey4[i] == 0:
+        zeroTot += 1
+
+print(zeroTot)
+
+
+"""
+spidey2Mat = np.matrix(spidey2)
+with open('matfile1.txt', 'w') as f:
+    for line in spidey2Mat:
+        np.savetxt(f, line, fmt='%i')
+
+spidey3Mat = np.matrix(spidey3)
+with open('matfile1.txt', 'w') as f:
+    for line in spidey3Mat:
+        np.savetxt(f, line, fmt='%i')
+
+spidey4Mat = np.matrix(spidey4)
+with open('matfile1.txt', 'w') as f:
+    for line in spidey4Mat:
+        np.savetxt(f, line, fmt='%i')
+"""
